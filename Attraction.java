72a5f55d055534da82b46ea2d3c072dd648e770e
zandercoffman <zandergalvez330@gmail.com>
2023-10-25 21:24:55 +0000

    WIP

---
+++ Attraction.java
@@ -0,0 +1,55 @@
+/**
+*   A project done for Mr. Heiser's class that is an Attraction and 
+*   numerous classes extend that, and there are 3 classes, one for each of our
+*   partners.
+*
+*   @author Jacob Pape
+*   @author Zander Coffman 
+*   @author Vy Huynh
+*/
+
+public class Attraction
+{
+  //all variables that a attraction would need
+  private int utilitiecost;
+  private int maxRiders;
+  private int numtickets;
+  private int ticketCost;
+  private int ticketAmount;
+  private int attendants;
+  private int speed;
+  private int height;
+  private int ridetime;
+  
+  
+  /**
+  *  @return maxRiders
+  */
+  public int returnMaxRiders() {return maxRiders;}
+  /**
+  *  @return numtickets
+  */
+  public int returnNumTickets() {return numtickets;}
+  /**
+  *  @return ticketCost
+  */
+  public int returnTicketCost() {return ticketCost;}
+  /**
+  *  @return ticketAmount
+  */
+  public int returnTicketAmount() {return ticketAmount;}
+  /**
+  *  @return attendant
+  */
+  public int returnAttendants() {return attendants;}
+  /** 
+  *  @return speed
+  */
+  public int returnSpeed() {return speed;}
+  
+  public int returnHeight() {return height;}
+  public int returnRideTime() {return rideTime;}
+  public int returnUtilitieCost() {return utilitiecost;}
+  
+} 
+
